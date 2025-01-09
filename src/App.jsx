cb985386c91c298d7e0369aaf603c6d6b6ffa954
Jamoliddin Muminov <jmuminov92@gmail.com>
2025-01-08 21:49:26 -0500

    completed first puppyBowl workshop

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
+import { puppyList } from "./data.js";
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
+  function handleClick() {
+    // some logic here
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
-    <>
-      <div>
-      </div>  
-    </>
-  )
+    <div className="App">
+      <div className="puppy-grid">
+        {puppies.map((puppy) => {
+          return (
+            <div
+              className="puppy-cell"
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </div>
+          );
+        })}
+      </div>
+      {featPupId && (
+        <div className="featured-puppy">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
